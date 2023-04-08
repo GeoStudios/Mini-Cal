3f47d34e8f62885489cf5219f941a1bfaa8a06f4
Zombi <zombeifedgaming@outlook.com>
2023-04-07 21:55:38 -0500

    Gates Created

--- src/logic/gates.java
+++ src/logic/gates.java
@@ -1,4 +1,38 @@
 package logic;
 
 public class gates {
+
+    public static int not(int a){
+        if (a == 1) return 1; else return 0;
+    }
+
+    public static int buffer(int a){
+        return not(not(a));
+    }
+
+    public static int and(int a, int b){
+        if (a == 1 && b == 1) return 1; else return 0;
+    }
+
+    public static int nand(int a, int b){
+        return not(and(a, b));
+    }
+
+    public static int or(int a, int b){
+        return not(and(not(a), not(b)));
+    }
+
+    public static int nor(int a, int b){
+        return not(or(a, b));
+    }
+
+    public static int xor(int a, int b){
+        return or(and(a, not(b)), and(not(a), b));
+    }
+
+    public static int xnor(int a, int b){
+        return not(xor(a, b));
+    }
+
+
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,13 @@
+import logic.circuits.adder;
+import logic.circuits.subtractor;
+import logic.circuits.multiplier;
+import logic.circuits.divider;
+
+import logic.converter;
+
+
 public class main {
+
+
+
 }
